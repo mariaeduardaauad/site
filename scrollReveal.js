fc2c51eed21c9d98f7c753f14539ba80a62fab89
Davi Barbosa Bandeira <daviibandeiraa@gmail.com>
2023-04-18 23:36:21 -0300

    update

--- scrollReveal.js
+++ scrollReveal.js
@@ -1,6 +1,6 @@
 window.sr = ScrollReveal({ reset: true})
 
-ScrollReveal().reveal( '.area-0', { delay: 500 } )
+ScrollReveal().reveal( '.area-0', { delay: 400 } )
 ScrollReveal().reveal( '.area-1', { delay: 500 } )
 ScrollReveal().reveal( '.area-2', { delay: 500 }  )
 ScrollReveal().reveal( '.area-3', { delay: 500 }  )
